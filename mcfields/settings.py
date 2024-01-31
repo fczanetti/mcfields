737c6f59e2012a4d157dc3e8f7987ac28d090945
Fabio Zanetti <fczanetti@hotmail.com>
2024-01-31 16:22:17 -0300

    banco de dados criado e algumas configurações feitas

--- mcfields/settings.py
+++ mcfields/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv
+from dj_database_url import parse as db_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y!8=hh!iug@&c$*_6$m+3qp2&fv(=31a7211s5)f+7sssakxup'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -74,12 +76,9 @@ WSGI_APPLICATION = 'mcfields.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+SQLITE_DB = 'sqlite:///' + str(BASE_DIR / 'db.sqlite3')
+
+DATABASES = {'default': config('DATABASE_URL', cast=db_url)}
 
 
 # Password validation
