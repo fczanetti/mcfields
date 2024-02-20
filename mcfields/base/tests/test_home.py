9c6405dcdee481e237b99c13c38963ee5da9a669
Fabio Zanetti <fczanetti@hotmail.com>
2024-02-20 17:07:51 -0300

    alguns testes para home page adicionados e feitos alguns ajustes
    closes #16

--- mcfields/base/tests/test_home.py
+++ mcfields/base/tests/test_home.py
@@ -19,17 +19,20 @@ def test_status_code_home(resp):
     assert resp.status_code == 200
 
 
-def test_title_home_page(resp):
+def test_title_e_icone_home_page(resp):
     """
-    Certifica de que o título está presente na home page.
+    Certifica de que o título e o ícone estão presentes na home page.
     """
     assert_contains(resp, "<title>McField's</title>")
+    assert_contains(resp, '<link rel="icon" type="image/png" href="/static/base/img/logo_original_icon.png">')
 
 
 def test_nav_bar_home_page(resp):
     """
     Certifica de que a navbar e seus componentes estão presentes na home page.
     """
+    assert_contains(resp, f'<a href="{reverse("base:home")}" id="logo-nav-bar"><img '
+                          f'src="/static/base/img/logo-red-2.jpg" alt="Logotipo"></a>')
     assert_contains(resp, '<button class="toggle-button">')
     assert_contains(resp, '<div class="navbar-links">')
     assert_contains(resp, '<li><a href="">Sobre</a></li>')
@@ -42,6 +45,7 @@ def test_top_image_div_home_page(resp):
     """
     Certifica de que os componentes da primeira div (topo da página) estão presentes na home page
     """
+    assert_contains(resp, '<div id="top-image-div" style="background-image: url(/static/base/img/top_picture.jpg)">')
     assert_contains(resp, '<div id="top-message">')
 
 
@@ -50,7 +54,8 @@ def test_about_section_home_page(resp):
     Certifica de que os componentes da div "about-consultant" estão presentes na home page.
     """
     assert_contains(resp, '<div id="about-consultant">')
-    assert_contains(resp, '<img id="about-consultant-img"')
+    assert_contains(resp, '<img id="about-consultant-img" src="/static/base/img/foto_victor_02.png" '
+                          'alt="Foto do logotipo">')
     assert_contains(resp, '<div id="about-text-div">')
     assert_contains(resp, '<h1 id="about-consultant-title">Sobre o consultor</h1>')
     assert_contains(resp, '<p id="about-consultant-text">')
@@ -62,18 +67,43 @@ def test_our_services_home_page(resp):
     """
     assert_contains(resp, '<div id="our-services">')
     assert_contains(resp, '<div id="our-services-div">')
-    assert_contains(resp, '<h1 id="our-services-title"')
+    assert_contains(resp, '<h1 id="our-services-title" >Serviços prestados</h1>')
     assert_contains(resp, '<p id="our-services-text">')
     assert_contains(resp, '<div id="slider">')
     assert_contains(resp, '<div class="slide">')
-    assert_contains(resp, '<h3 class="slide-title">')
-    assert_contains(resp, '<p class="text-slide">')
+    assert_contains(resp, '<img src="/static/base/img/abacate.jpg" alt="Foto relacionada ao serviço prestado">')
+    assert_contains(resp, '<img src="/static/base/img/colheita.jpg" alt="Foto relacionada ao serviço prestado">')
+    assert_contains(resp, '<img src="/static/base/img/exportacao.jpg" alt="Foto relacionada ao serviço prestado">')
+    assert_contains(resp, '<img src="/static/base/img/plantacao.jpg" alt="Foto relacionada ao serviço prestado">')
+    assert_contains(resp, '<h3 class="slide-title">Título do serviço</h3>')
+    assert_contains(resp, '<h3 class="slide-title">Título do serviço</h3>')
+    assert_contains(resp, '<h3 class="slide-title">Título do serviço</h3>')
+    assert_contains(resp, '<h3 class="slide-title">Título do serviço</h3>')
 
 
 def test_mais_infos_home_page(resp):
     """
     Certifica de que os componentes da div 'mais infos' estão presentes na home page.
     """
-    assert_contains(resp, '<div id="mais-infos"')
+    assert_contains(resp, '<div id="mais-infos" style="background-image: url(/static/base/img/background03.jpg)">')
     assert_contains(resp, '<div id="mais-infos-bg">')
     assert_contains(resp, '<div id="mais-infos-text">')
+
+
+def test_footer_home_page(resp):
+    """
+    Certifica de que os componentes do footer estão presentes.
+    """
+    assert_contains(resp, '<footer id="footer">')
+    assert_contains(resp, '<div id="logo-div">')
+    assert_contains(resp, '<img id="logo-footer" src="/static/base/img/logo_branco.png" alt="Logotipo">')
+    assert_contains(resp, '<label id="label-email-form" for="email-input">Fique por dentro:</label>')
+    assert_contains(resp, '<input id="email-input" type="text" placeholder="example@gmail.com" name="email">')
+    assert_contains(resp, '<input id="input-button" type="submit" value="Inscrever-se">')
+
+
+def test_scripts_home_page(resp):
+    """
+    Certifica de que os scripts necessários para algumas funcionalidades da home page estão presentes.
+    """
+    assert_contains(resp, '<script src="/static/base/js/script.js" ></script>')
