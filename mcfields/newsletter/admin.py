c1c89e45e746e87875987eda22bee8b5c0bbf21e
Fabio Zanetti <fczanetti@hotmail.com>
2024-02-25 11:28:34 -0300

    Modelo de newsletter criado e migrações aplicadas
    close #50

--- mcfields/newsletter/admin.py
+++ mcfields/newsletter/admin.py
@@ -1,3 +1,8 @@
-# from django.contrib import admin
+from django.contrib import admin
+from mcfields.newsletter.models import Newsletter
 
-# Register your models here.
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ['title', 'intro', 'pub_date', 'edit_date', 'author', 'slug']
+    prepopulated_fields = {'slug': ['title']}
--- mcfields/newsletter/tests/test_indice_newsletter.py
+++ mcfields/newsletter/tests/test_indice_newsletter.py
@@ -1,9 +1,23 @@
 import pytest
 from django.urls import reverse
+from model_bakery import baker
+from datetime import date
+from mcfields.django_assertions import assert_contains
+from mcfields.newsletter.models import Newsletter
 
 
 @pytest.fixture
-def resp_indice_newsletters(client):
+def newsletters(db):
+    """
+    Cria algumas newsletters para serem apresentadas na página de índice.
+    :return: Newsletters criadas.
+    """
+    news = baker.make(Newsletter, _quantity=3)
+    return news
+
+
+@pytest.fixture
+def resp_indice_newsletters(client, newsletters):
     """
     Cria uma requisição na página de índice de newsletters.
     """
@@ -16,3 +30,15 @@ def test_status_code_indice_newsletter(resp_indice_newsletters):
     Certifica de que a página de índice de newsletters foi carregada com sucesso.
     """
     assert resp_indice_newsletters.status_code == 200
+
+
+def test_dados_newsletter_indice(resp_indice_newsletters, newsletters):
+    """
+    Certifica de que as informações de cada newsletter estão presentes na página de índice.
+    """
+    for newsletter in newsletters:
+        pub_date = date.strftime(newsletter.pub_date, "%d/%m/%Y")
+        assert_contains(resp_indice_newsletters, newsletter.title)
+        assert_contains(resp_indice_newsletters, newsletter.intro)
+        assert_contains(resp_indice_newsletters, newsletter.author)
+        assert_contains(resp_indice_newsletters, pub_date)
