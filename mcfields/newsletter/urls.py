4c9115f9c3a79bf814fcff18c2a7db479d687593
Fabio Zanetti <fczanetti@hotmail.com>
2024-02-26 15:11:46 -0300

    Página de detalhes da newsletter criada
    close #52

--- mcfields/newsletter/urls.py
+++ mcfields/newsletter/urls.py
@@ -6,4 +6,5 @@ app_name = 'newsletter'
 urlpatterns = [
     path('', views.indice_newsletters, name='indice_newsletters'),
     path('<slug:slug>', views.detalhe_newsletter, name='detalhe_newsletter'),
+    path('adm/post', views.post_newsletter, name='post'),
 ]
