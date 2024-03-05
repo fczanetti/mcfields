cfece395e8d285b87cbb4491744eb42b6ad22259
Fabio Zanetti <fczanetti@hotmail.com>
2024-03-04 22:12:17 -0300

    Template de adição de serviços adicionado
    closes #132

--- mcfields/servicos/urls.py
+++ mcfields/servicos/urls.py
@@ -3,5 +3,6 @@ from mcfields.servicos import views
 
 app_name = 'servicos'
 urlpatterns = [
-    path('<slug:slug>', views.detalhe_servico, name='detalhe_servico')
+    path('<slug:slug>', views.detalhe_servico, name='detalhe_servico'),
+    path('adm/adicionar', views.adicionar_servico, name='adicionar'),
 ]
