f8006d6dcee0805238a945a3a4b2e2d2f5a8c072
Fabio Zanetti <fczanetti@hotmail.com>
2024-03-07 18:15:34 -0300

    Botões de adição e edição inseridos na página de detalhes de serviço
    closes #125, closes #124

--- mcfields/servicos/tests/test_pag_detalhe_servicos.py
+++ mcfields/servicos/tests/test_pag_detalhe_servicos.py
@@ -2,7 +2,7 @@ import pytest
 from django.urls import reverse
 from model_bakery import baker
 
-from mcfields.django_assertions import assert_contains
+from mcfields.django_assertions import assert_contains, assert_not_contains
 from mcfields.servicos.models import Servico
 
 
@@ -21,6 +21,36 @@ def resp_pagina_detalhe_servico(client, servico):
     Cria uma requisição na página de detalhes de serviços com usuário não logado.
     """
     resp = client.get(reverse('servicos:detalhe_servico', args=(servico.slug,)))
+    return resp
+
+
+@pytest.fixture
+def resp_pag_det_servico_usuario_logado_sem_perm(client_usuario_logado, servico):
+    """
+    Cria uma requisição na página de detalhes de serviços
+    com usuário logado e sem permissão de adição nem edição.
+    """
+    resp = client_usuario_logado.get(reverse('servicos:detalhe_servico', args=(servico.slug,)))
+    return resp
+
+
+@pytest.fixture
+def resp_pag_det_serv_usuario_log_com_perm_adic(client_usuario_log_com_perm_adic_serv, servico):
+    """
+    Cria uma requisição na página de detalhes de serviços com
+    usuário logado e com permissão de adição.
+    """
+    resp = client_usuario_log_com_perm_adic_serv.get(reverse('servicos:detalhe_servico', args=(servico.slug,)))
+    return resp
+
+
+@pytest.fixture
+def resp_pag_det_serv_usuario_log_com_perm_edic(client_usuario_log_com_perm_edic_serv, servico):
+    """
+    Cria uma requisição na página de detalhes de serviços com
+    usuário logado e com permissão de edição.
+    """
+    resp = client_usuario_log_com_perm_edic_serv.get(reverse('servicos:detalhe_servico', args=(servico.slug,)))
     return resp
 
 
@@ -44,3 +74,43 @@ def test_infos_pag_detalhe_servico(resp_pagina_detalhe_servico, servico):
     """
     assert_contains(resp_pagina_detalhe_servico, f'<h1 id="service-title">{servico.title}</h1>')
     assert_contains(resp_pagina_detalhe_servico, servico.content)
+
+
+def test_botao_edicao_e_adicao_servico_usuario_nao_logado(resp_pagina_detalhe_servico, servico):
+    """
+    Certifica de que os botões de edição e adição de serviço não estão presentes.
+    """
+    assert_not_contains(resp_pagina_detalhe_servico, f'<a id="service-update-link" '
+                                                     f'href="{servico.get_edition_url()}">Editar</a>')
+    assert_not_contains(resp_pagina_detalhe_servico, f'<a id="service-adit-link" '
+                                                     f'href="{reverse("servicos:adicionar")}">Novo serviço</a>')
+
+
+def test_botao_edic_e_adic_serv_usuario_log_sem_perm(resp_pag_det_servico_usuario_logado_sem_perm, servico):
+    """
+    Certifica de que os botões de edição e adição de serviço não estão presentes.
+    """
+    assert_not_contains(resp_pag_det_servico_usuario_logado_sem_perm, f'<a id="service-update-link" '
+                                                                      f'href="{servico.get_edition_url()}">Editar</a>')
+    assert_not_contains(resp_pag_det_servico_usuario_logado_sem_perm, f'<a id="service-adit-link" '
+                                                                      f'href="{reverse("servicos:adicionar")}">'
+                                                                      f'Novo serviço</a>')
+
+
+def test_botao_adic_usuario_logado_com_perm_adic(resp_pag_det_serv_usuario_log_com_perm_adic):
+    """
+    Certifica de que o botão de adição de serviço está presente
+    para o usuário logado com permissão.
+    """
+    assert_contains(resp_pag_det_serv_usuario_log_com_perm_adic, f'<a id="service-adit-link" '
+                                                                 f'href="{reverse("servicos:adicionar")}">'
+                                                                 f'Novo serviço</a>')
+
+
+def test_botao_edic_usuario_logado_com_perm_edic(resp_pag_det_serv_usuario_log_com_perm_edic, servico):
+    """
+    Certifica de que o botão de edição de serviço está presente
+    para o usuário logado com permissão.
+    """
+    assert_contains(resp_pag_det_serv_usuario_log_com_perm_edic, f'<a id="service-update-link" '
+                                                                 f'href="{servico.get_edition_url()}">Editar</a>')
