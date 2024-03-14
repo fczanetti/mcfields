3e48657559efd2dd3cca537dda12b93da5b7c1eb
Fabio Zanetti <fczanetti@hotmail.com>
2024-03-14 13:24:54 -0300

    Edição de vídeo inserida
    closes #180

--- mcfields/videos/tests/conftest.py
+++ mcfields/videos/tests/conftest.py
@@ -1,8 +1,17 @@
 import pytest
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
-
+from model_bakery import baker
 from mcfields.videos.models import Video
+
+
+@pytest.fixture
+def video(db):
+    """
+    Cria e retorna um video.
+    """
+    vid = baker.make(Video, title='Video 01')
+    return vid
 
 
 @pytest.fixture
@@ -23,4 +32,25 @@ def client_usuario_log_com_perm_adic_video(usuario_senha_plana_com_perm_adic_vid
     Cria um client com usuário logado e permissão de adição de videos.
     """
     client.force_login(usuario_senha_plana_com_perm_adic_video)
+    return client
+
+
+@pytest.fixture
+def usuario_senha_plana_com_perm_edic_video(usuario_senha_plana):
+    """
+    Cria um usuário com permissão de edição de vídeos.
+    """
+    content_type = ContentType.objects.get_for_model(Video)
+    permission = Permission.objects.get(codename='change_video', content_type=content_type)
+    usuario_senha_plana.user_permissions.add(permission)
+    usuario_com_perm_edicao = usuario_senha_plana
+    return usuario_com_perm_edicao
+
+
+@pytest.fixture
+def client_usuario_log_com_perm_edic_video(usuario_senha_plana_com_perm_edic_video, client):
+    """
+    Cria um client com usuário logado e permissão de edição de videos.
+    """
+    client.force_login(usuario_senha_plana_com_perm_edic_video)
     return client
--- mcfields/videos/urls.py
+++ mcfields/videos/urls.py
@@ -5,5 +5,6 @@ app_name = 'videos'
 urlpatterns = [
     path('', views.indice_videos, name='indice'),
     path('adm/post', views.post_video, name='post'),
+    path('adm/edicao/<int:id>', views.edicao_video, name='edicao'),
     path('<slug:subject_slug>/<slug:slug>', views.detalhe_video, name='detalhe_video'),
 ]
