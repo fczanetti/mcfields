d311d6ffddeab2843f53745384eef15fcfc6ced2
Fabio Zanetti <fczanetti@hotmail.com>
2024-03-14 10:52:15 -0300

    Botão de adição de vídeos inserido
    closes #178

--- mcfields/videos/tests/test_indice_videos.py
+++ mcfields/videos/tests/test_indice_videos.py
@@ -2,7 +2,7 @@ import pytest
 from django.urls import reverse
 from model_bakery import baker
 from mcfields.base.models import Subject
-from mcfields.django_assertions import assert_contains
+from mcfields.django_assertions import assert_contains, assert_not_contains
 from mcfields.videos.models import Video
 
 
@@ -32,6 +32,26 @@ def resp_indice_videos_usuario_nao_logado(client, db):
     Realiza uma requisição na página de índice de vídeos com usuário não logado.
     """
     resp = client.get(reverse('videos:indice'))
+    return resp
+
+
+@pytest.fixture
+def resp_indice_videos_usuario_log_sem_perm_adic(client_usuario_logado):
+    """
+    Realiza uma requisição na página de índice de vídeos com usuário logado
+    sem permissão de adição.
+    """
+    resp = client_usuario_logado.get(reverse('videos:indice'))
+    return resp
+
+
+@pytest.fixture
+def resp_indice_videos_usuario_log_com_perm_adic(client_usuario_log_com_perm_adic_video):
+    """
+    Realiza uma requisição na página de índice de vídeos com usuário logado
+    com permissão de adição.
+    """
+    resp = client_usuario_log_com_perm_adic_video.get(reverse('videos:indice'))
     return resp
 
 
@@ -71,3 +91,30 @@ def test_links_videos_indice_videos(criar_videos, resp_indice_videos_usuario_nao
     """
     for video in criar_videos:
         assert_contains(resp_indice_videos_usuario_nao_logado, video.get_absolute_url())
+
+
+def test_botao_adicao_video_usuario_nao_logado(resp_indice_videos_usuario_nao_logado):
+    """
+    Certifica de que o botão de adição de vídeo não está disponível
+    para o usuário não logado.
+    """
+    assert_not_contains(resp_indice_videos_usuario_nao_logado, f'<a id="video-pub-link" '
+                                                               f'href="{reverse("videos:post")}">Novo vídeo</a>')
+
+
+def test_botao_adicao_video_usuario_log_sem_perm(resp_indice_videos_usuario_log_sem_perm_adic):
+    """
+    Certifica de que o botão de adição de vídeo não está disponível
+    para o usuário logado sem permissão.
+    """
+    assert_not_contains(resp_indice_videos_usuario_log_sem_perm_adic, f'<a id="video-pub-link" '
+                                                                      f'href="{reverse("videos:post")}">Novo vídeo</a>')
+
+
+def test_botao_adicao_video_usuario_log_com_perm(resp_indice_videos_usuario_log_com_perm_adic):
+    """
+    Certifica de que o botão de adição de vídeo está disponível
+    para o usuário logado com permissão.
+    """
+    assert_contains(resp_indice_videos_usuario_log_com_perm_adic, f'<a id="video-pub-link" '
+                                                                  f'href="{reverse("videos:post")}">Novo vídeo</a>')
