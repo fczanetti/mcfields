a88dd67073023fb36bb5c718e1c361798c56eb81
Fabio Zanetti <fczanetti@hotmail.com>
2024-03-22 13:46:21 -0300

    Política de privacidade adicionada
    closes #217

--- mcfields/base/urls.py
+++ mcfields/base/urls.py
@@ -4,6 +4,7 @@ from mcfields.base import views
 app_name = 'base'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('politica_privacidade', views.politica_privac, name='politica_privacidade'),
     path('sobre', views.sobre, name='sobre'),
     path('assuntos', views.subjects, name='subjects'),
     path('assuntos/adm/adicao', views.adic_subject, name='adic_subject'),
